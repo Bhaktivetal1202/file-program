bb09975b1f5dbdc734b51089032f1c26ba8af5ee
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2023-09-23 19:51:16 +0530

    Add files via upload

---
+++ f3.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+int main()
+{
+	FILE *fp1,*fp2;
+	char ch;
+	fp1=fopen("bhakti.txt","r");
+	fp2=fopen("vetal.txt","w");
+	if(fp1==NULL)
+	{
+		printf("file is not found");
+		exit(0);
+	}
+	while(!feof(fp1))
+	{
+		ch=fgetc(fp1);
+		if(isupper(ch))
+		fputc(tolower(ch),fp2);
+	   else	if(islower(ch))
+		fputc(toupper(ch),fp2);
+		else if(isdigit(ch))
+		fputc('*',fp2);
+		else
+		fputc(ch,fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	printf("file copy successfullyy..");
+}
